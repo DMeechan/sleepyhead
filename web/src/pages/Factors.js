a5abc10f3ea6f686e54af4fd36e3a275602e4969
Adam Binks <jellyberg@gmail.com>
2019-10-18 20:01:03 +0100

    Add factor status page wip

--- web/src/pages/Factors.js
+++ web/src/pages/Factors.js
@@ -2,9 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Content from '../components/Content';
+import FactorStatus from '../components/FactorStatus';
+
+class FactorGrid extends React.Component {
+    render() {
+        return <div id="factor-grid" className="ui grid">
+            <div className="four wide column">
+                <FactorStatus score="30" factor="Temperature" colour_class="red-factor" />
+            </div>
+            <div className="four wide column">
+                <FactorStatus score="75" factor="Factor 2" colour_class="green-factor" />
+            </div>
+            <div className="four wide column">
+                <FactorStatus score="75" factor="Factor 3" colour_class="yellow-factor" />
+            </div>
+            <div className="four wide column">
+                <FactorStatus score="75" factor="Factor 4" colour_class="green-factor" />
+            </div>
+        </div>
+    }
+}
 
 export default class Factors extends React.Component {
   render() {
-    return <Content text="factors" />;
+    return <div id="content-container">
+        <FactorGrid />
+    </div>;
   }
 }
