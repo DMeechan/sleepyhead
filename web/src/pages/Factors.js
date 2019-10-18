09d0402cff8861cbaf37bdcea3aa0862a0895388
Adriana <adrianacucu99@gmail.com>
2019-10-18 22:07:16 +0100

    Added back button

--- web/src/pages/Factors.js
+++ web/src/pages/Factors.js
@@ -4,28 +4,55 @@ import { Link } from 'react-router-dom';
 import FactorStatus from '../components/FactorStatus';
 
 class FactorGrid extends React.Component {
-    render() {
-        return <div id="factor-grid" className="ui grid">
-            <div className="four wide column">
-                <FactorStatus score="30" factor="Temperature" colour_class="red-factor" />
-            </div>
-            <div className="four wide column">
-                <FactorStatus score="75" factor="Factor 2" colour_class="green-factor" />
-            </div>
-            <div className="four wide column">
-                <FactorStatus score="75" factor="Factor 3" colour_class="yellow-factor" />
-            </div>
-            <div className="four wide column">
-                <FactorStatus score="75" factor="Factor 4" colour_class="green-factor" />
-            </div>
+  render() {
+    return (
+      <div id="factor-grid" className="ui grid">
+        <div className="four wide column">
+          <FactorStatus
+            score="30"
+            factor="Temperature"
+            colour_class="red-factor"
+          />
         </div>
-    }
+        <div className="four wide column">
+          <FactorStatus
+            score="75"
+            factor="Factor 2"
+            colour_class="green-factor"
+          />
+        </div>
+        <div className="four wide column">
+          <FactorStatus
+            score="75"
+            factor="Factor 3"
+            colour_class="yellow-factor"
+          />
+        </div>
+        <div className="four wide column">
+          <FactorStatus
+            score="75"
+            factor="Factor 4"
+            colour_class="green-factor"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default class Factors extends React.Component {
   render() {
-    return <div id="content-container">
-        <FactorGrid />
-    </div>;
+    return (
+      <div>
+        <Link to="/">
+          <button id="back-button" class="circular ui icon huge button">
+            <i class="arrow alternate left icon"></i>
+          </button>
+        </Link>
+        <div id="content-container">
+          <FactorGrid />
+        </div>
+      </div>
+    );
   }
 }
--- web/src/pages/Feedback.js
+++ web/src/pages/Feedback.js
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default class Feedback extends React.Component {
   render() {
-    return <div id="content-container">Feedback</div>;
+    return (
+      <div>
+        <Link to="/factors">
+          <button id="back-button" class="circular ui icon huge button">
+            <i class="arrow alternate left icon"></i>
+          </button>
+        </Link>
+        <div id="content-container">Feedback</div>
+      </div>
+    );
   }
 }
