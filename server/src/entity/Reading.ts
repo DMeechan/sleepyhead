48047e180717344f2876a9a80b7390f3f101f69e
Daniel Meechan <daniel@Daniel-MBP.local>
2019-10-18 22:48:25 +0100

    Set up entities

---
+++ server/src/entity/Reading.ts
@@ -0,0 +1,55 @@
+import { Entity, Column, CreateDateColumn, ManyToOne, Index } from "typeorm";
+import { SleepCycle } from "./SleepCycle";
+import { User } from "./User";
+
+export enum ReadingType {
+  TEMPERATURE = "temperature",
+  LUMINOSITY = "luminosity",
+  NOISE = "noise"
+}
+
+@Entity()
+export class Reading {
+  @Column({ primary: true })
+  id: number;
+
+  @Column()
+  value: number;
+
+  @Column({
+    type: "enum",
+    enum: ReadingType
+  })
+  type: ReadingType;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @Index()
+  @ManyToOne(type => SleepCycle, sleepCycle => sleepCycle.readings)
+  sleepCycle: SleepCycle;
+}
+
+export function createReading(data: {
+  type: ReadingType;
+  value: number;
+}): Reading {
+  const reading = new Reading();
+
+  switch (data.type) {
+    case "temperature":
+      reading.type = ReadingType.TEMPERATURE;
+      break;
+    case "luminosity":
+      reading.type = ReadingType.LUMINOSITY;
+      break;
+    case "noise":
+      reading.type = ReadingType.NOISE;
+      break;
+    default:
+      throw new Error("reading type not recognised: " + data.type);
+  }
+
+  reading.value = data.value;
+  return reading;
+}
---
+++ server/src/entity/SleepCycle.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  Generated,
+  CreateDateColumn,
+  Index,
+  OneToMany
+} from "typeorm";
+import { User } from "./User";
+import { Reading } from "./Reading";
+
+@Entity()
+export class SleepCycle {
+  @Column({ primary: true })
+  id: number;
+
+  @Column()
+  @Generated("uuid")
+  uuid: string;
+
+  @Column()
+  quality: number;
+
+  @Index()
+  @CreateDateColumn()
+  createdAt: string;
+
+  @Index()
+  @Column()
+  endedAt: string;
+
+  @Index()
+  @ManyToOne(type => User, user => user.sleepCycles)
+  user: User;
+
+  @OneToMany(type => Reading, reading => reading.sleepCycle)
+  readings: Reading[];
+}
