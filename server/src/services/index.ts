2d34ebc9c104da2802c6fcd95a897e9327c4f915
Daniel Meechan <daniel@visitor19.gu-visitors.net>
2019-10-18 16:20:07 +0100

    Added basic endpoints

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,3 +1,6 @@
 import searchRoutes from "./search/routes";
+import sensorsRoutes from "./sensors/routes";
+import analysisRoutes from "./analysis/routes";
+import healthRoutes from "./health/routes";
 
-export default [...searchRoutes];
+export default [...sensorsRoutes, ...analysisRoutes, ...healthRoutes];
