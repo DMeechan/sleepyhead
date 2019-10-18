2ce4cd5952cc74d16536d3673974d266d13c5529
Adriana <adrianacucu99@gmail.com>
2019-10-18 15:43:11 +0100

    Fixed directories web

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
+
+export default App;
