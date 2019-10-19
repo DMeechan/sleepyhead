e378b4b3b5bbaaaceb3440ad29d334d11b393000
Adriana <adrianacucu99@gmail.com>
2019-10-19 11:10:26 +0100

    Added login page

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -3,8 +3,23 @@ import React from 'react';
 export default class Login extends React.Component {
   render() {
     return (
-      <div id="content-container">
-        hello u needa login
+      <div id="login-container">
+        <form className="ui form center aligned container">
+          <div className="field">
+            <div className="ui transparent input">
+              <input
+                id="input-text"
+                type="text"
+                placeholder="Enter username..."
+                minLength="2"
+                maxLength="10"
+              />
+            </div>
+          </div>
+            <button id="login-button" className="ui button" type="submit">
+              Create user / Log in
+            </button>
+        </form>
       </div>
     );
   }
