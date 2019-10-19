a6e678a44629d12c0bc47517168a588f575e1aaf
Adam Binks <jellyberg@gmail.com>
2019-10-19 11:52:33 +0100

    Added response parsing and hooked up to bar chart

--- web/src/components/BarChart.js
+++ web/src/components/BarChart.js
@@ -2,40 +2,40 @@ import React, { Component } from 'react';
 import { Bar } from 'react-chartjs-2';
 import 'chartjs-plugin-datalabels';
 
-const weekday = new Array(7);
-weekday[0] = 'Sunday';
-weekday[1] = 'Monday';
-weekday[2] = 'Tuesday';
-weekday[3] = 'Wednesday';
-weekday[4] = 'Thursday';
-weekday[5] = 'Friday';
-weekday[6] = 'Saturday';
+// const weekday = new Array(7);
+// weekday[0] = 'Sunday';
+// weekday[1] = 'Monday';
+// weekday[2] = 'Tuesday';
+// weekday[3] = 'Wednesday';
+// weekday[4] = 'Thursday';
+// weekday[5] = 'Friday';
+// weekday[6] = 'Saturday';
+//
+// const getWeekday = (today, i) => {
+//   return today - i >= 0 ? today - i : 7 + (today - i);
+// };
+//
+// const weekDays = () => {
+//   const labels = new Array(7);
+//   for (let i = 0; i <= 6; i += 1) {
+//     labels[i] = weekday[getWeekday(new Date().getDay(), i)];
+//   }
+//   return labels.reverse();
+// };
 
-const getWeekday = (today, i) => {
-  return today - i >= 0 ? today - i : 7 + (today - i);
-};
-
-const weekDays = () => {
-  const labels = new Array(7);
-  for (let i = 0; i <= 6; i += 1) {
-    labels[i] = weekday[getWeekday(new Date().getDay(), i)];
-  }
-  return labels.reverse();
-};
-
-const data = {
-  labels: weekDays(),
-  datasets: [
-    {
-      backgroundColor: '#ec8b5e',
-      borderColor: '#ec8b5e',
-      borderWidth: 0.1,
-      hoverBackgroundColor: 'rgba(236,139,94,0.7)',
-      hoverBorderColor: '#ec8b5e',
-      data: [43, 60.5, 12.76, 85, 100, 30, 67, 0, 101]
-    }
-  ]
-};
+// const data = {
+//   labels: weekDays(),
+//   datasets: [
+//     {
+//       backgroundColor: '#ec8b5e',
+//       borderColor: '#ec8b5e',
+//       borderWidth: 0.1,
+//       hoverBackgroundColor: 'rgba(236,139,94,0.7)',
+//       hoverBorderColor: '#ec8b5e',
+//       data: [43, 60.5, 12.76, 85, 100, 30, 67, 0, 101]
+//     }
+//   ]
+// };
 
 export default class BarChart extends Component {
   render() {
@@ -47,7 +47,17 @@ export default class BarChart extends Component {
             console.log(elems[0]._datasetIndex + ', ' + elems[0]._index);
             window.location = './factors';
           }}
-          data={data}
+          data={{
+            datasets: [{
+                backgroundColor: '#ec8b5e',
+                borderColor: '#ec8b5e',
+                borderWidth: 0.1,
+                hoverBackgroundColor: 'rgba(236,139,94,0.7)',
+                hoverBorderColor: '#ec8b5e',
+                data: this.props.chartData.y
+            }],
+            labels: this.props.chartData.x
+          }}
           width={100}
           height={50}
           options={{
