1648772fa63c6c1695786d156f9547c4128b4516
Adriana <adrianacucu99@gmail.com>
2019-10-19 10:37:57 +0100

    Added redirects to login

--- web/src/pages/Overview.js
+++ web/src/pages/Overview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import BarChart from '../components/BarChart';
 
-export default class Home extends React.Component {
+export default class Overview extends React.Component {
   render() {
     return (
       <div id="content-container">
